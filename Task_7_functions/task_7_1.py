e9a2f521a73ac8afca1bb74418bddadc15bad5a7
qlixxxy <sqlixor@gmail.com>
2021-11-15 12:02:23 +0300

    completed task

--- Task_7_functions/task_7_1.py
+++ Task_7_functions/task_7_1.py
@@ -28,4 +28,43 @@ def function9(x):
 def function10(x):
     return x / 3.785
 
+def function11(x):
+    return x * 1.759
 
+def function12(x):
+    return x / 1.759
+
+def main():
+    while True:
+        y = int(input('Введите номер функции от 1-12. 0 - завершить программу'))
+        if y == 0:
+            return 'Программа завершена'
+        x = float(input('Введите значение, которое желаете сконвертировать'))
+        if y == 1:
+            print(function1(x))
+        elif y == 2:
+            print(function2(x))
+        elif y == 3:
+            print(function3(x))
+        elif y == 4:
+            print(function4(x))
+        elif y == 5:
+            print(function5(x))
+        elif y == 6:
+            print(function6(x))
+        elif y == 7:
+            print(function7(x))
+        elif y == 8:
+            print(function8(x))
+        elif y == 9:
+            print(function9(x))
+        elif y == 10:
+            print(function10(x))
+        elif y == 11:
+            print(function11(x))
+        elif y == 12:
+            print(function12(x))
+        else:
+            print('Введен неккоректный номер функции')
+
+print(main())
