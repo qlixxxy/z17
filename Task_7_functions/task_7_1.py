fbbc2cf9618de29af21995e52175f7758f436d72
qlixxxy <sqlixor@gmail.com>
2021-11-15 11:32:41 +0300

    added 1st task

---
+++ Task_7_functions/task_7_1.py
@@ -0,0 +1,31 @@
+def function1(x):
+    return x * 2.54 
+
+def function2(x):
+    return x / 2.54
+
+def function3(x):
+    return x * 1.609
+
+def function4(x):
+    return x / 1.609
+
+def function5(x):
+    return x / 2.205
+
+def function6(x):
+    return x * 2.205
+
+def function7(x):
+    return x * 28.35
+
+def function8(x):
+    return x / 28.35
+
+def function9(x):
+    return x * 3.785
+
+def function10(x):
+    return x / 3.785
+
+
